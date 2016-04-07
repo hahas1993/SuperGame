de6fb5c1a7dd319ba220203f04904d94a975c261
Pawel <p.matwiejuk1993@gmail.com>
2016-04-07 15:54:51 +0200

    opencv added

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -1,10 +1,18 @@
 package com.mygdx.game;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
@@ -14,5 +22,11 @@ public class AndroidLauncher extends AndroidApplication {
 		config.useAccelerometer = false;
 		config.useCompass = false;
 		initialize(new MyGdxGame(), config);
+
+		if (!OpenCVLoader.initDebug()) {
+			Log.e(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
+		} else {
+			Log.d(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), working.");
+		}
 	}
 }
