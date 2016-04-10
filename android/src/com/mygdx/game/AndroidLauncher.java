4e606053b048ec4f1e0e3a74b8684f42d1cd03b0
Pawel <p.matwiejuk1993@gmail.com>
2016-04-10 16:28:27 +0200

    opencv eye tracking

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -5,14 +5,8 @@ import android.util.Log;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
 
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Mat;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
-
-import java.io.File;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
