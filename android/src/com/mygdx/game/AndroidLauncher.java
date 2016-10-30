28d3cfbffe18e61bfe5be54f6857c45897a6a774
Pawel <p.matwiejuk1993@gmail.com>
2016-10-30 20:47:46 +0100

    added screen support (pseudo menu)

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -1,11 +1,8 @@
 package com.mygdx.game;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
 
@@ -38,12 +35,6 @@ public class AndroidLauncher extends AndroidApplication implements CameraBridgeV
 
 	private int learn_frames = 0;
 
-	private MenuItem mItemFace50;
-	private MenuItem mItemFace40;
-	private MenuItem mItemFace30;
-	private MenuItem mItemFace20;
-	private MenuItem mItemType;
-
 	private Mat mRgba;
 	private Mat mGray;
 
@@ -52,7 +43,6 @@ public class AndroidLauncher extends AndroidApplication implements CameraBridgeV
 	private CascadeClassifier mJavaDetectorEye;
 
 
-	private int mDetectorType = JAVA_DETECTOR;
 	private String[] mDetectorName;
 
 	private float mRelativeFaceSize = 0.2f;
@@ -296,37 +286,4 @@ public class AndroidLauncher extends AndroidApplication implements CameraBridgeV
 		return null;
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		Log.i(TAG, "called onCreateOptionsMenu");
-		mItemFace50 = menu.add("Face size 50%");
-		mItemFace40 = menu.add("Face size 40%");
-		mItemFace30 = menu.add("Face size 30%");
-		mItemFace20 = menu.add("Face size 20%");
-		mItemType = menu.add(mDetectorName[mDetectorType]);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		Log.i(TAG, "called onOptionsItemSelected; selected item: " + item);
-		if (item == mItemFace50)
-			setMinFaceSize(0.5f);
-		else if (item == mItemFace40)
-			setMinFaceSize(0.4f);
-		else if (item == mItemFace30)
-			setMinFaceSize(0.3f);
-		else if (item == mItemFace20)
-			setMinFaceSize(0.2f);
-		else if (item == mItemType) {
-			int tmpDetectorType = (mDetectorType + 1) % mDetectorName.length;
-			item.setTitle(mDetectorName[tmpDetectorType]);
-		}
-		return true;
-	}
-
-	private void setMinFaceSize(float faceSize) {
-		mRelativeFaceSize = faceSize;
-		mAbsoluteFaceSize = 0;
-	}
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,303 +1,43 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.TimeUtils;
 
-import java.util.Iterator;
+public class MyGdxGame extends Game {
 
-public class MyGdxGame extends ApplicationAdapter {
-
-	private Texture coinImage;
-	private int coinWidth;
-	private int coinHeight;
-	private TextureRegion[] coinFrames;
-	private Texture asteroidImage;
-	private int asteroidWidth;
-	private int asteroidHeight;
-	private TextureRegion[] asteroidFrames;
-	private TextureRegion currentFrame;
-	private Texture playerShipImage;
-	//private Sound dropSound;
-	//private Music rainMusic;
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Rectangle playerShip;
-	private Array<Animation> coins;
-	private Array<Animation> asteroids;
-	private Array<Rectangle> coinsR;
-	private Array<Rectangle> asteroidsR;
-	private long lastCoinTime;
-	private long lastAsteroidTime;
-
-	private double directionX = 0;
-
-	private final int START_LIFE = 5;
-	private final int START_SCORE = 0;
-	private int life = START_LIFE;
-	private int score = START_SCORE;
-	CharSequence lifeString = String.valueOf(START_LIFE);
-	CharSequence scoreString = String.valueOf(START_SCORE);
+	SpriteBatch batch;
 	BitmapFont font;
+	MenuScreen menuScreen;
+	GameScreen gameScreen;
 
-	boolean pause = true;
-	boolean end = false;
-	private float stateTime;
-
-	@Override
-	public void create () {
-		coinImage = new Texture(Gdx.files.internal("coin.png"));
-		coinHeight = coinImage.getHeight();
-		coinWidth = coinImage.getWidth()/8;
-		TextureRegion[][] tmp = TextureRegion.split(coinImage, coinWidth, coinHeight);
-		coinFrames = new TextureRegion[8];
-		int index = 0;
-		for (int i = 0; i < 1; i++) {
-			for (int j = 0; j < 8; j++) {
-				coinFrames[index++] = tmp[i][j];
-			}
-		}
-
-		asteroidImage = new Texture(Gdx.files.internal("asteroid.png"));
-		asteroidHeight = asteroidImage.getHeight()/4;
-		asteroidWidth = asteroidImage.getWidth()/5;
-		tmp = TextureRegion.split(asteroidImage, asteroidWidth, asteroidHeight);
-		asteroidFrames = new TextureRegion[19];
-		index = 0;
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 5; j++) {
-				if (i < 3 || j < 4)
-					asteroidFrames[index++] = tmp[i][j];
-			}
-		}
-
-		playerShipImage = new Texture(Gdx.files.internal("playerShip.png"));
-
-		//dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
-		//rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
-
-		//rainMusic.setLooping(true);
-		//rainMusic.play();
-
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
+	public void create() {
 		batch = new SpriteBatch();
-
-		playerShip = new Rectangle();
-		playerShip.x = 800/ 2 - playerShipImage.getWidth() / 2;
-		playerShip.y = 20;
-		playerShip.width = playerShipImage.getWidth();
-		playerShip.height = playerShipImage.getHeight();
-
-		coins = new Array<Animation>();
-		asteroids = new Array<Animation>();
-		coinsR = new Array<Rectangle>();
-		asteroidsR = new Array<Rectangle>();
-
 		font = new BitmapFont();
-		font.getData().setScale(2);
-
-		stateTime = 0f;
+		menuScreen = new MenuScreen(this);
+		gameScreen = new GameScreen(this);
+		this.setScreen(menuScreen);
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0.1f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.setProjectionMatrix(camera.combined);
-
-		lifeString = String.valueOf(life);
-		scoreString = String.valueOf(score);
-
-		if(pause) {
-			batch.begin();
-			font.draw(batch, lifeString, 30, 460);
-			font.draw(batch, scoreString, 770, 460);
-			font.draw(batch, "head your face on screen center", 200, 240);
-			batch.draw(playerShipImage, playerShip.x, playerShip.y);
-			batch.end();
-			if(Gdx.input.isTouched()) {
-				restart();
-			}
-			return;
-		}
-
-		if(end){
-			batch.begin();
-			font.draw(batch, "final score: " + scoreString, 350, 240);
-			batch.end();
-			if(Gdx.input.isTouched()) {
-				restart();
-			}
-			return;
-		}
-
-		stateTime += Gdx.graphics.getDeltaTime();
-		batch.begin();
-		font.draw(batch, lifeString, 30, 460);
-		font.draw(batch, scoreString, 770, 460);
-		batch.draw(playerShipImage, playerShip.x, playerShip.y);
-		int index = 0;
-		for(Animation coinAnimation : coins) {
-			currentFrame = coinAnimation.getKeyFrame(stateTime, true);
-			batch.draw(currentFrame, coinsR.get(index).x, coinsR.get(index).y);
-			index ++;
-		}
-		index = 0;
-		for(Animation asteroidAnimation : asteroids) {
-			currentFrame = asteroidAnimation.getKeyFrame(stateTime, true);
-			batch.draw(currentFrame, asteroidsR.get(index).x, asteroidsR.get(index).y);
-			index ++;
-		}
-		batch.end();
-
-		if(Math.abs(directionX) > 18) {
-			playerShip.x += directionX / 8;
-		}
-
-		if(playerShip.x < 0)
-			playerShip.x = 0;
-		if(playerShip.x > 800 - playerShip.getWidth())
-			playerShip.x = 800 - playerShip.getWidth();
-		if(playerShip.y < 0)
-			playerShip.y = 0;
-		if(playerShip.y > 480 - playerShip.getHeight())
-			playerShip.y = 480 - playerShip.getHeight();
-
-		if(TimeUtils.nanoTime() - lastCoinTime > 1000000000)
-			spawnCoin();
-
-		if(TimeUtils.nanoTime() - lastAsteroidTime > 1500000000)
-			spawnAsteroid();
-
-		Iterator<Rectangle> iter = coinsR.iterator();
-		Iterator<Animation> iterA = coins.iterator();
-		while(iter.hasNext()) {
-			Rectangle coin = iter.next();
-			iterA.next();
-			coin.y -= 50 * Gdx.graphics.getDeltaTime();
-			if (coin.y + coinHeight < 0) {
-				iter.remove();
-				iterA.remove();
-			}
-			if(coin.overlaps(playerShip)) {
-				//dropSound.play();
-				score ++;
-				iter.remove();
-				iterA.remove();
-			}
-		}
-
-		iter = asteroidsR.iterator();
-		iterA = asteroids.iterator();
-		while(iter.hasNext()) {
-			Rectangle asteroid = iter.next();
-			iterA.next();
-			asteroid.y -= 120 * Gdx.graphics.getDeltaTime();
-			if (asteroid.y + asteroidHeight < 0) {
-				iter.remove();
-				iterA.remove();
-			}
-			if(asteroid.overlaps(playerShip)) {
-				//dropSound.play();
-				life --;
-				iter.remove();
-				iterA.remove();
-				if(life <= 0){
-					life = 0;
-					end = true;
-				}
-			}
-		}
+	public void render() {
+		super.render(); // important!
 	}
 
-	private void restart() {
-		life = START_LIFE;
-		score = START_SCORE;
-		lifeString = String.valueOf(START_LIFE);
-		scoreString = String.valueOf(START_SCORE);
-		end = false;
-		pause = false;
-
-		Iterator<Rectangle> iter = coinsR.iterator();
-		while(iter.hasNext()) {
-			iter.next();
-			iter.remove();
-		}
-		Iterator<Animation> iterA = coins.iterator();
-		while(iterA.hasNext()) {
-			iterA.next();
-			iterA.remove();
-		}
-		iter = asteroidsR.iterator();
-		while(iter.hasNext()) {
-			iter.next();
-			iter.remove();
-		}
-		iterA = asteroids.iterator();
-		while(iterA.hasNext()) {
-			iterA.next();
-			iterA.remove();
-		}
-
-
-		playerShip.x = 800/ 2 - playerShip.getWidth() / 2;
-		playerShip.y = 20;
-	}
-
-	public void setDirectionX(double directionX){
-		this.directionX = directionX;
-	}
-
-	@Override
 	public void dispose() {
-		coinImage.dispose();
-		asteroidImage.dispose();
-		playerShipImage.dispose();
-		//dropSound.dispose();
-		//rainMusic.dispose();
 		batch.dispose();
+		font.dispose();
 	}
 
-	private void spawnCoin() {
-		Rectangle coin = new Rectangle();
-		coin.x = MathUtils.random(0, 800 - coinWidth);
-		coin.y = 480;
-		coin.width = coinWidth;
-		coin.height = coinHeight;
-		coinsR.add(coin);
-		coins.add(new Animation(0.025f, coinFrames));
-		lastCoinTime = TimeUtils.nanoTime();
-	}
-
-	private void spawnAsteroid() {
-		Rectangle asteroid = new Rectangle();
-		asteroid.x = MathUtils.random(0, 800 - asteroidWidth);
-		asteroid.y = 480;
-		asteroid.width = asteroidWidth;
-		asteroid.height = asteroidHeight;
-		asteroidsR.add(asteroid);
-		asteroids.add(new Animation(0.025f, asteroidFrames));
-		lastAsteroidTime = TimeUtils.nanoTime();
+	public void setDirectionX(double directionX) {
+		gameScreen.setDirectionX(directionX);
 	}
 
 	public boolean isPause() {
-		return pause;
+		return gameScreen.isPause();
 	}
 
 	public void setPause(boolean pause) {
-		this.pause = pause;
+		gameScreen.setPause(pause);
 	}
+
 }
